42470d0ed4d5c27a65562a62426d8989ad0485fb
Keiji93 <84074860+Keiji93@users.noreply.github.com>
2026-03-18 18:29:54 +0100

    Configure dotenv for local development

--- server.ts
+++ server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { createServer as createViteServer } from 'vite';
 import path from 'path';
 import parseHandler from './api/parse.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function startServer() {
   const app = express();
